0e643d40289e1bc54f33c055f8ab7e9f49935897
Aga <aga.k396@gmail.com>
2020-03-05 11:45:26 +0100

    Initialize repository

---
+++ lib/aga_palindrome.rb
@@ -0,0 +1,6 @@
+require "aga_palindrome/version"
+
+module AgaPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/aga_palindrome/version.rb
@@ -0,0 +1,3 @@
+module AgaPalindrome
+  VERSION = "0.1.0"
+end
